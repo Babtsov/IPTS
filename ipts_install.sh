3833ecfdadef314f34b77b426870853e22fab92c
Ben Babtsov <ben.babtsov@ufl.edu>
2016-10-14 22:35:15 -0400

    create 2 containers and touch 1 file in each

--- ipts_install.sh
+++ ipts_install.sh
@@ -1,3 +1,13 @@
+echo "Installing dependencies..."
 apk add lxc lxc-templates bridge
-lxc-create -t alpine -n provisioning
-lxc-start --name provisioning
+apk add expect
+echo "Creating containers..."
+lxc-create -t alpine -n sip
+lxc-create -t alpine -n sipmedia
+lxc-start --name sip
+lxc-start --name sipmedia
+echo "configuring sip container..."
+expect ./sip_config.exp
+echo "configuring sipmedia container..."
+expect ./sipmedia_config.exp
+echo "DONE with IPTS INSTALL"
