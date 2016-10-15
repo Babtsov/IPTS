990d89f7b0db58ad2d40ff5051d8a32b0a139447
Babtsov <ben.babtsov@ufl.edu>
2016-10-15 03:26:01 +0000

    Make the output of ipts_install nicer

--- ipts_install.sh
+++ ipts_install.sh
@@ -6,8 +6,11 @@ lxc-create -t alpine -n sip
 lxc-create -t alpine -n sipmedia
 lxc-start --name sip
 lxc-start --name sipmedia
+if [ ! -d log ]; then
+    mkdir log
+fi
 echo "configuring sip container..."
-expect ./sip_config.exp
+expect ./sip_config.exp >> ./log/sip_config.log
 echo "configuring sipmedia container..."
-expect ./sipmedia_config.exp
+expect ./sipmedia_config.exp >> ./log/sipmedia_config.log
 echo "DONE with IPTS INSTALL"
