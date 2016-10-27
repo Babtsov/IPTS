9444527c971e8f166f8d5e3790af15dcf8c3c2da
root <root@lxc-host.my.domain>
2016-10-27 00:59:59 +0000

    creates the sip container neccesary though manually have to restart the service when you console in

--- install.sh
+++ install.sh
@@ -1,28 +1,32 @@
+echo "restarting networking"
+service networking restart
+
 echo "updating & upgrading APK"
-apk update && apk upgrades
+apk update && apk upgrade
 
 echo "adding git"
 apk add git
 
-echo "backing up to LBU"
-lbu commit
+echo "backing up LBU --- not really(removed for now)"
 
 echo "adding bash, bridge, and vim"
 apk add bash bridge vim
 
 echo "configuring networking on host..."
 rm /etc/network/interfaces
+touch /etc/network/interfaces
 cp host_config/interfaces /etc/network/interfaces
 service networking restart
+
 echo "testing networking..."
-ping www.google.com -t 3
+wget www.google.com
 
 
 echo "adding lxc dependencies..."
 apk add lxc lxc-templates
 echo "creating lxc configuration..."
 cp host_config/lxc.conf /etc/lxc/lxc.conf
-service networking restar
+service networking restart
 
 
 echo "creating sip container..."
@@ -33,9 +37,10 @@ echo "Sip Container created"
 echo "including /var/lib/lxc in LBU"
 lbu include /var/lib/lxc
 echo "Starting sip container..."
-lxc-start -name sip
+lxc-start --name sip
 
-cp sip_config /var/lib/lxc/sip/rootfs/tmp/
+cp -r sip_config /var/lib/lxc/sip/rootfs/tmp/
 echo "executing sip_config inside container..."
 lxc-attach -n sip -- /tmp/sip_config/config.sh
+lxc-attach -n sip -- /etc/init.d/networking restart
 echo "done with config.sh inside sip"
--- sip_config/config.sh
+++ sip_config/config.sh
@@ -1,6 +1,4 @@
 #!/bin/sh
 rm /etc/network/interfaces
-cp sip_config/network_interfaces /etc/network/interfaces
-service networking start
-rc-update add networking
-ping www.google.com -t 3
+touch /etc/network/interfaces
+cp /tmp/sip_config/network_interfaces /etc/network/interfaces
