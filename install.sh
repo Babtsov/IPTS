cd55aeb5f89eea6e22cac45a67503d8ad2eb0985
dmathew93 <denzel.mathew@gmail.com>
2016-10-27 00:01:15 -0400

    configured sip script

--- install.sh
+++ install.sh
@@ -1,27 +1,41 @@
-echo "Installing dependencies..."
+echo "updating & upgrading APK"
+apk update && apk upgrades
+
+echo "adding git"
+apk add git
+
+echo "backing up to LBU"
+lbu commit
+
+echo "adding bash, bridge, and vim"
+apk add bash bridge vim
+
+echo "configuring networking on host..."
+rm /etc/network/interfaces
+cp host_config/interfaces /etc/network/interfaces
+service networking restart
+echo "testing networking..."
+ping www.google.com -t 3
+
+
+echo "adding lxc dependencies..."
 apk add lxc lxc-templates
-
-echo "Transferring lxc configuration files..."
+echo "creating lxc configuration..."
 cp host_config/lxc.conf /etc/lxc/lxc.conf
-cat /host_config/lxc.comf
-echo "Restarting network service..."
-/etc/init.d/networking restart
-echo "Network service restarted."
+service networking restar
 
-# workaround to ensure we can use lxc-attach
-# see https://wiki.alpinelinux.org/wiki/Talk:LXC
+
+echo "creating sip container..."
 echo 0 > /proc/sys/kernel/grsecurity/chroot_caps
 echo 0 > /proc/sys/kernel/grsecurity/chroot_deny_chroot
-
-echo "Creating and configuring the sip container..."
 lxc-create -n sip -f /etc/lxc/lxc.conf -t alpine
-lxc-start --name sip
+echo "Sip Container created"
+echo "including /var/lib/lxc in LBU"
+lbu include /var/lib/lxc
+echo "Starting sip container..."
+lxc-start -name sip
 
-cp -r sip_config /var/lib/lxc/sip/rootfs/tmp/
-echo "executing config.sh inside sip..."
+cp sip_config /var/lib/lxc/sip/rootfs/tmp/
+echo "executing sip_config inside container..."
 lxc-attach -n sip -- /tmp/sip_config/config.sh
 echo "done with config.sh inside sip"
-echo "configuring sipmedia container..."
-lxc-create -n sipmedia -f /etc/lxc/lxc.conf -t alpine
-lxc-start --name sipmedia
-echo "DONE with IPTS INSTALL"
--- sip_config/config.sh
+++ sip_config/config.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
-/etc/init.d/networking start
+rm /etc/network/interfaces
+cp sip_config/network_interfaces /etc/network/interfaces
+service networking start
 rc-update add networking
-ping www.google.com
+ping www.google.com -t 3
