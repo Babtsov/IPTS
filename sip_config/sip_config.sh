0c85bf6be6e9b790a955c9d75db8dffe784e7d0e
Ben Babtsov <ben.babtsov@ufl.edu>
2016-12-08 17:20:02 -0500

    fixed the path for sip_network_inferfaces

--- sip_config/sip_config.sh
+++ sip_config/sip_config.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 rm /etc/network/interfaces
 touch /etc/network/interfaces
-cp /tmp/sip_config/sip_network_interfaces /etc/network/interfaces
+cp /root/sip_config/sip_network_interfaces /etc/network/interfaces
 service networking restart
 apk add acf-postgresql
