bf0d0a9b187d73b97d9908a7012bb8946de9fc99
Ben Babtsov <ben.babtsov@ufl.edu>
2016-12-09 00:59:57 -0500

    add some more functionality to the provisioning script

--- provisioning_config/provisioning_config.sh
+++ provisioning_config/provisioning_config.sh
@@ -6,3 +6,18 @@ cp /root/provisioning_config/provisioning_network_interfaces /etc/network/interf
 service networking restart
 
 rc-update add networking
+
+# Configure remote administration
+apk update && apk upgrade
+setup-sshd -c openssh 
+apk add postgresql-dev python python-dev libpq gcc build-base zlib postgresql postgresql-client
+
+# Setup postgresql
+/etc/init.d/postgresql setup
+sed "/^[# ]*log_destination/clog_destination = 'syslog'" -i /var/lib/postgresql/9.3/data/postgresql.conf
+/etc/init.d/postgresql start
+rc-update add postgresql
+wget http://initd.org/psycopg/tarballs/PSYCOPG-2-5/psycopg2-2.5.2.tar.gz
+tar -xzvf psycopg2-2.5.2.tar.gz
+cd psycopg2-2.5.2
+python setup.py install
