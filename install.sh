571b14adda6b0e1bc0a4d1c2026568c33f95c175
dmathew93 <denzel.mathew@gmail.com>
2016-12-10 21:55:59 -0500

    added more logging everywhere

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ cp host_config/resolv.conf /etc/resolv.conf
 cp host_config/interfaces /etc/network/interfaces
 service networking restart
 # configure the apk tool to an endpoint where it will fetch Alpine Linux packages
-echo 1 | /media/usb/IPTS/setup-apkrepos
+echo 1 | /media/usb/IPTS/setup-apkrepos > /dev/null
 echo "updating & upgrading apk"
 apk update && apk upgrade
 
@@ -20,43 +20,47 @@ echo 0 > /proc/sys/kernel/grsecurity/chroot_deny_chroot
 
 echo "CREATING a log directory"
 mkdir ~/log
+touch ~/log/host_config.log # create a log for the host_config itself
 # ------------- sip config ------------------
-echo "CREATING sip container..."
-lxc-create -n sip -f /etc/lxc/lxc.conf -t alpine
-echo "STARTING sip container..."
-lxc-start --name sip
-echo "TRANSFERRING script files into sip container"
+echo "CREATING sip container..." | tee ~/log/host_config.log
+lxc-create -n sip -f /etc/lxc/lxc.conf -t alpine >> ~/log/host_config.log
+echo "STARTING sip container..." | tee ~/log/host_config.log
+lxc-start --name sip 2>&1 | tee -a ~/log/host_config.log
+echo "TRANSFERRING script files into sip container" | tee ~/log/host_config.log
 cp -r sip_config /var/lib/lxc/sip/rootfs/root/
-echo "CONFIGURRING sip container"
+echo "CONFIGURRING sip container" | tee ~/log/host_config.log
 lxc-attach -n sip -e -- /root/sip_config/sip_config.sh > ~/log/sip_config.log 2> ~/log/sip_config.error
-echo "DONE configuring sip. (see log at ~/log/sip_config.log)"
+echo "DONE configuring sip. (see log at ~/log/sip_config.log)" | tee ~/log/host_config.log
 # ------------- sipmedia config -------------
-echo "CREATING sipmedia container..."
-lxc-create -n sipmedia -f /etc/lxc/lxc.conf -t alpine
-echo "STARTING sip media container..."
-lxc-start --name sipmedia
-echo "TRANSFERRING script files into sipmedia container"
+echo "CREATING sipmedia container..." | tee ~/log/host_config.log
+lxc-create -n sipmedia -f /etc/lxc/lxc.conf -t alpine >> ~/log/host_config.log
+echo "STARTING sip media container..." | tee ~/log/host_config.log
+lxc-start --name sipmedia sipmedia 2>&1 | tee ~/log/host_config.log
+echo "TRANSFERRING script files into sipmedia container" | tee ~/log/host_config.log
 cp -r sipmedia_config /var/lib/lxc/sipmedia/rootfs/root/
+echo "CONFIGURRING sip media container" | tee ~/log/host_config.log
 lxc-attach -n sipmedia -e -- /root/sipmedia_config/sipmedia_config.sh > ~/log/sipmedia_config.log 2> ~/log/sipmedia_config.error
-echo "DONE configuring sipmedia. (see log at ~/log/sipmedia_config.log)"
+echo "DONE configuring sipmedia. (see log at ~/log/sipmedia_config.log)" | tee ~/log/host_config.log
 # ------------- dhcpdns config -------------
-echo "CREATING dhcpdns container..."
-lxc-create -n dhcpdns -f /etc/lxc/lxc.conf -t alpine
-echo "STARTING dhcpdns container..."
-lxc-start --name dhcpdns
-echo "TRANSFERRING script files into dhcpdns container"
+echo "CREATING dhcpdns container..." | tee ~/log/host_config.log
+lxc-create -n dhcpdns -f /etc/lxc/lxc.conf -t alpine >> ~/log/host_config.log
+echo "STARTING dhcpdns container..." | tee ~/log/host_config.log
+lxc-start --name dhcpdns 2>&1 | tee ~/log/host_config.log
+echo "TRANSFERRING script files into dhcpdns container" | tee ~/log/host_config.log
 cp -r dhcpdns_config /var/lib/lxc/dhcpdns/rootfs/root/
+echo "CONFIGURRING dhcpdns container" | tee ~/log/host_config.log
 lxc-attach -n dhcpdns -e -- /root/dhcpdns_config/dhcpdns_config.sh > ~/log/dhcpdns_config.log 2> ~/log/dhcpdns_config.error
-echo "DONE configuring dhcpdns. (see log at ~/log/dhcpdns_config.log)"
+echo "DONE configuring dhcpdns. (see log at ~/log/dhcpdns_config.log)" | tee ~/log/host_config.log
 # ------------- provisioning config --------
-echo "CREATING provisioning container..."
-lxc-create -n provisioning -f /etc/lxc/lxc.conf -t alpine
-echo "STARTING provisioning container..."
-lxc-start --name provisioning
-echo "TRANSFERRING script files into provisioning container"
+echo "CREATING provisioning container..." | tee ~/log/host_config.log
+lxc-create -n provisioning -f /etc/lxc/lxc.conf -t alpine >> ~/log/host_config.log
+echo "STARTING provisioning container..." | tee ~/log/host_config.log
+lxc-start --name provisioning 2>&1 | tee ~/log/host_config.log
+echo "TRANSFERRING script files into provisioning container" | tee ~/log/host_config.log
 cp -r provisioning_config /var/lib/lxc/provisioning/rootfs/root/
+echo "CONFIGURRING provisioning container" | tee ~/log/host_config.log
 lxc-attach -n provisioning -e -- /root/provisioning_config/provisioning_config.sh > ~/log/provisioning_config.log 2> ~/log/provisioning_config.error
-echo "DONE configuring provisioning. (see log at ~/log/provisioning_config.log)"
+echo "DONE configuring provisioning. (see log at ~/log/provisioning_config.log)" | tee ~/log/host_config.log
 # ------------- debugging config ------------
 echo "Configuring system to fasciliate debugging..."
 ./release.sh 		# make USB writeable
