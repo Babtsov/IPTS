0820dc0cd328fb1dbe63d638e03434d04cdb7fbf
Ben Babtsov <ben.babtsov@ufl.edu>
2016-12-12 18:57:14 -0500

    fix spacing, add comments, and remove unnecessary parts

--- install.sh
+++ install.sh
@@ -1,25 +1,24 @@
 echo "updating network settings to activate bridge"
-apk add bridge
+apk add bridge # make sure we add bridge support before configuring network interfaces
 cp host_config/resolv.conf /etc/resolv.conf
 cp host_config/interfaces /etc/network/interfaces
 /etc/init.d/networking start
-# configure the apk tool to an endpoint where it will fetch Alpine Linux packages
-echo 1 | /media/usb/IPTS/host_config/setup-apkrepos > /dev/null
+# configure the apk tool to an endpoint where it will fetch Alpine Linux packages & fetch most recent packages
+echo 1 | /media/usb/IPTS/host_config/setup-apkrepos > /dev/null # and do so quietly
 echo "updating & upgrading apk"
 apk update && apk upgrade
 
+echo "CONFIGURING remote SSH access"
+mkdir ~/.ssh
+cat host_config/ipts.pub >> ~/.ssh/authorized_keys   # append our public key to authorized keys
+chmod 600 ~/.ssh/authorized_keys && chmod 700 ~/.ssh # adjust premissions so ssh doesn't complain
+
 echo "adding lxc dependencies..."
 apk add lxc lxc-templates
-
 echo "CREATING lxc configuration..."
 cp host_config/lxc.conf /etc/lxc/lxc.conf
 
-echo "CONFIGURING remote SSH access"
-mkdir ~/.ssh
-cat host_config/ipts.pub >> ~/.ssh/authorized_keys # append our public key to authorized keys
-chmod 600 ~/.ssh/authorized_keys && chmod 700 ~/.ssh # adjust premissions so ssh doesn't complain
-
-# a workaround that allows us to run lxc-attach
+# a workaround that allows us to run lxc-attach (see https://wiki.alpinelinux.org/wiki/Talk:LXC)
 echo 0 > /proc/sys/kernel/grsecurity/chroot_caps
 echo 0 > /proc/sys/kernel/grsecurity/chroot_deny_chroot
 
@@ -69,11 +68,7 @@ echo "DONE configuring provisioning. (see log at ~/log/provisioning_config.log)"
 # ------------- debugging config ------------
 echo "Configuring system to fasciliate debugging..."
 
-./release.sh 			# make USB writeable
-apk add git bash vim	# add git bash and vim
-
-
-# ------------- ssh config ------------
-./serverSSH.sh
+./release.sh            # make USB writeable
+apk add git bash vim    # add git bash and vim
 
 echo "CONFIGURATION DONE"
