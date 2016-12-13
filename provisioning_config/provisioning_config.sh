ea3021b10a4b99660ae89ca5bae83f8767933083
Ben Babtsov <ben.babtsov@ufl.edu>
2016-12-13 16:53:49 -0500

    add the provisioning module to ACF

--- provisioning_config/provisioning_config.sh
+++ provisioning_config/provisioning_config.sh
@@ -30,7 +30,9 @@ cp /root/provisioning_config/postgresql.conf /var/lib/postgresql/9.5/data/postgr
 lbu include /usr/lib/python2.7/
 /etc/init.d/postgresql restart
 echo nameserver 10.2.0.1 > /etc/resolv.conf
+echo 'CONFIGURING ACF modules'
 setup-acf
+apk add acf-provisioning                      # add the provisioning module to ACF
 cp /root/provisioning_config/passwd /etc/acf/ # add an account to ACF so we can login
 echo -e "list\nlist" | passwd                 # set the default password of root to "list"
 mkdir /etc/ssl/mini_httpd
